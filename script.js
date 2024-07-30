62450aa100020274371d0921c9e7f9a1260325d7
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-07-30 10:27:12 -0400

    Starter Code with TODOs Uploaded

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Get references */
+
+/* SET PDIV AND IMGDIV DISPLAY STYLES OR ELSE IT WON'T WORK */
+
+/* Event Listener for pButton */
+
+// If pDiv is hidden, show it and hide the other div
+
+// Create a new paragraph element
+
+//Change the background and text color of this p element
+
+// Add padding and a margin as well to match demo
+
+// Add element to pDiv
+
+/* Event Listener for imgButton */
+
+// If imgDiv is hidden, show it and hide the other div
+
+// Create a new image element
+
+// Add Pikachu (or whatever image you choose) to the image
+
+// Add this image to your imgDiv
