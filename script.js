a9d7d6378b1a175fcd51fd1d4c74b9f2c8c9c853
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2024-07-30 10:43:28 -0400

    Extra Credit Uploaded

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 /* Get references */
 let pButton = document.getElementById('p-button');
 let imgButton = document.getElementById('img-button');
+let nothingButton = document.getElementById('nothing-button');
 
 let pDiv = document.getElementById('p-div');
 let imgDiv = document.getElementById('img-div');
+let nothingDiv = document.getElementById('nothing-div');
 
-/* SET PDIV AND IMGDIV DISPLAY STYLES OR ELSE IT WON'T WORK */
+let pCountSpan = document.getElementById('p-count');
+let imgCountSpan = document.getElementById('img-count');
+let nothingCountSpan = document.getElementById('nothing-count');
+
+/* SET DISPLAY STYLES OR ELSE IT WON'T WORK */
 pDiv.style.display = 'block';
 imgDiv.style.display = 'none';
+nothingDiv.style.display = 'none';
+
+/* Set initial counts */
+let pCount = 0;
+let imgCount = 0;
+let nothingCount = 0;
+
+// Make sure your spans reflect this
+pCountSpan.innerHTML = pCount;
+imgCountSpan.innerHTML = imgCount;
+nothingCountSpan.innerHTML = nothingCount;
 
 /* Event Listener for pButton */
 pButton.onclick = function () {
-  // If pDiv is hidden, show it and hide the other div
+  // If pDiv is hidden, show it and hide the other divs
   if (pDiv.style.display == 'none') {
-    pDiv.style.display = 'block';
     imgDiv.style.display = 'none';
+    nothingDiv.style.display = 'none';
+    pDiv.style.display = 'block';
   }
 
   // Create a new paragraph element
@@ -31,13 +49,18 @@ pButton.onclick = function () {
 
   // Add element to pDiv
   pDiv.append(newP);
+
+  // Increase pCount
+  pCount++;
+  pCountSpan.innerHTML = pCount;
 };
 
 /* Event Listener for imgButton */
 imgButton.onclick = function () {
-  // If imgDiv is hidden, show it and hide the other div
+  // If imgDiv is hidden, show it and hide the other divs
   if (imgDiv.style.display == 'none') {
     pDiv.style.display = 'none';
+    nothingDiv.style.display = 'none';
     imgDiv.style.display = 'block';
   }
 
@@ -48,4 +71,22 @@ imgButton.onclick = function () {
 
   // Add this image to your imgDiv
   imgDiv.append(newImg);
+
+  // Increase imgCount
+  imgCount++;
+  imgCountSpan.innerHTML = imgCount;
+};
+
+/* Do Nothing Event Listener */
+nothingButton.onclick = function () {
+  // If nothingDiv is hidden, show it and hide other divs
+  if (nothingDiv.style.display == 'none') {
+    pDiv.style.display = 'none';
+    imgDiv.style.display = 'none';
+    nothingDiv.style.display = 'block';
+  }
+
+  // Increase nothingCount
+  nothingCount++;
+  nothingCountSpan.innerHTML = nothingCount;
 };
